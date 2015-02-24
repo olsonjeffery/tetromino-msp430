ed9d2d4fafc9d1c5c3a7b85ade1563a76e7efaff
Jeff Olson <olson.jeffery@gmail.com>
2015-02-23 21:42:08 -0600

    starting to sketch out game logic

---
+++ src/tetromino.c
@@ -0,0 +1,10 @@
+// Copyright (c) 2015 Jeff Olson <olson.jeffery@gmail.com>
+// This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+// See http://creativecommons.org/licenses/by-nc-sa/4.0/ for more information
+//
+// tetris.c - tetris game logic/state impl
+
+// This is the tetris game field.. each entry is a "column"
+// with "rightward" bits being more "downward" from bits to their left.
+// ...way longer than it needs to be.
+static long field[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
---
+++ src/tetromino.h
@@ -0,0 +1,5 @@
+// Copyright (c) 2015 Jeff Olson <olson.jeffery@gmail.com>
+// This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+// See http://creativecommons.org/licenses/by-nc-sa/4.0/ for more information
+//
+// tetromino.h - tetromino game API
